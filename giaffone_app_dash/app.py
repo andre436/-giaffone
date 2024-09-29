77da912e8c5c4091d395b11aa456543a3d8d9545
ANDRERDS <63514585+andre436@users.noreply.github.com>
2024-09-28 23:14:02 -0300

    Update app.py

--- giaffone_app_dash/app.py
+++ giaffone_app_dash/app.py
@@ -53,10 +53,7 @@ login_layout = dbc.Container([
     dbc.Row(dbc.Col(dbc.Input(id="username", placeholder="Usuário", type="text"), width=12)),
     dbc.Row(dbc.Col(dbc.Input(id="password", placeholder="Senha", type="password"), width=12)),
     dbc.Row(dbc.Col(dbc.Button("Acessar", id="login-button", color="primary", className="mt-3"), width=12)),
-    dbc.Row(dbc.Col(html.Div(id="login-output", className="mt-2 text-danger"), width=12)),
-    
-    # Adiciona dashboard-container ao layout inicial, mas oculto
-    dbc.Row(dbc.Col(html.Div(id='dashboard-container', style={'display': 'none'}), width=12))
+    dbc.Row(dbc.Col(html.Div(id="login-output", className="mt-2 text-danger"), width=12))
 ], className="mt-5")
 
 # Layout principal do aplicativo
@@ -90,26 +87,23 @@ main_layout = dbc.Container([
 
 # Função de callback para lidar com o login
 @app.callback(
-    Output('login-output', 'children', allow_duplicate=True),
-    Output('dashboard-container', 'style', allow_duplicate=True),
+    Output('login-output', 'children'),
+    Output(dash.dependencies.ALL, 'style'),  # Alterar o layout de forma geral
     Input('login-button', 'n_clicks'),
     State('username', 'value'),
     State('password', 'value'),
-    prevent_initial_call='initial_duplicate'  # Permitir duplicatas nas chamadas iniciais
+    prevent_initial_call=True
 )
 def login(n_clicks, username, password):
-    if n_clicks:
-        if username == "scat" and password == "1234":  # Simples validação de login
-            return f"Bem-vindo, {username}!", {'display': 'block'}
-        else:
-            return "Usuário ou senha inválidos", {'display': 'none'}
-    return "", {'display': 'none'}
+    if username == "scat" and password == "1234":  # Simples validação de login
+        return "", {'display': 'none'}, {'display': 'block'}  # Redireciona para o layout principal
+    else:
+        return "Usuário ou senha inválidos", {'display': 'none'}, {'display': 'none'}
 
 # Função para atualizar o gráfico com base no circuito selecionado
 @app.callback(
     Output('modal', 'is_open', allow_duplicate=True),
     Output('modal-content', 'children', allow_duplicate=True),
-    Output('dashboard-container', 'style', allow_duplicate=True),
     Input('btn-campo-grande', 'n_clicks'),
     Input('btn-goiania', 'n_clicks'),
     Input('btn-londrina', 'n_clicks'),
@@ -120,17 +114,16 @@ def login(n_clicks, username, password):
     Input('btn-curvelo', 'n_clicks'),
     Input('close-modal', 'n_clicks'),
     State('modal', 'is_open'),
-    State('dashboard-container', 'style'),
-    prevent_initial_call='initial_duplicate'  # Permitir duplicatas nas chamadas iniciais
+    prevent_initial_call=True
 )
-def display_dashboard(n_campo_grande, n_goiania, n_londrina, n_santa_cruz, n_interlagos, n_cascavel, n_taruma, n_curvelo, close_n_clicks, is_open, dashboard_style):
+def display_dashboard(n_campo_grande, n_goiania, n_londrina, n_santa_cruz, n_interlagos, n_cascavel, n_taruma, n_curvelo, close_n_clicks, is_open):
     ctx = dash.callback_context
     if not ctx.triggered:
-        return is_open, dash.no_update, dashboard_style
-    
+        return is_open, dash.no_update
+
     # Se o botão de fechar foi clicado, fecha a modal
     if ctx.triggered[0]['prop_id'].split('.')[0] == 'close-modal':
-        return False, dash.no_update, dashboard_style
+        return False, dash.no_update
 
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
     circuit = button_id.replace('btn-', '').replace('-', ' ').capitalize()
@@ -144,7 +137,7 @@ def display_dashboard(n_campo_grande, n_goiania, n_londrina, n_santa_cruz, n_int
     fig.add_trace(go.Scatter(x=time_data, y=temp_with, mode='lines', name='Com Arrefecimento'))
     fig.update_layout(title=f'Temperaturas durante a corrida em {circuit}', xaxis_title='Tempo (min)', yaxis_title='Temperatura (°C)')
 
-    return True, dcc.Graph(figure=fig), {'display': 'block'}
+    return True, dcc.Graph(figure=fig)
 
 # Definir o layout inicial como a página de login
 app.layout = login_layout
